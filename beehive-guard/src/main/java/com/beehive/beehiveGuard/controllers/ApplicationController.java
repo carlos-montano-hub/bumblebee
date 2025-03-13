83f91d4433e51f9f1c29da323573f447142d4e46
Luis Rubio <luirting@gmail.com>
2025-03-12 17:09:03 -0700

    add health check endpoint

---
+++ beehive-guard/src/main/java/com/beehive/beehiveGuard/controllers/ApplicationController.java
@@ -0,0 +1,14 @@
+package com.beehive.beehiveGuard.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class ApplicationController {
+
+    @GetMapping("/health")
+    public ResponseEntity<Void> healthCheck() {
+        return ResponseEntity.noContent().build();
+    }
+}
