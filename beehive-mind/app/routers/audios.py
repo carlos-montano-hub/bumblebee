33ac737b8774f2aefef3799b71cfe937245926d0
carlos-montano-hub <montanoc70@gmail.com>
2025-03-24 21:48:53 -0700

    refactor audio retrieval: update S3 file fetching and enhance response structure

--- beehive-mind/app/routers/audios.py
+++ beehive-mind/app/routers/audios.py
@@ -11,7 +11,12 @@ async def register(input: RegisterAudioForm):
     audio_id = input.audioId
 
     try:
-        file_data = s3_functions(settings.raw_bucket_name, audio_id)
-        return {"message": "File retrieved successfully", "file_size": len(file_data)}
+        file_data = s3_functions.get_file_from_s3(settings.raw_bucket_name, audio_id)
+        return_data = {
+            "message": "File retrieved successfully",
+            "file_size": file_data.frame_count(),
+        }
+        print(return_data)
+        return return_data
     except HTTPException as e:
         return {"error": e.detail}
