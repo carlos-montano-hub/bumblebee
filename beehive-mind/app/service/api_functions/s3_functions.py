361f6d575cbd14ca19545ce8198ff5c6015b5d2c
Luis Rubio <luirting@gmail.com>
2025-03-25 17:56:42 -0700

    fix: add missing s3 endpoint url

--- beehive-mind/app/service/api_functions/s3_functions.py
+++ beehive-mind/app/service/api_functions/s3_functions.py
@@ -8,6 +8,7 @@ from pydub import AudioSegment
 
 s3_client = boto3.client(
     "s3",
+    endpoint_url=settings.s3_endpoint,
     aws_access_key_id=settings.s3_access_key,
     aws_secret_access_key=settings.s3_secret_key,
     region_name=settings.s3_region,
